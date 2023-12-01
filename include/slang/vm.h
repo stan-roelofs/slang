e8ff8b85c79ab01011951a4ec00914d599f58ea5
Stan Roelofs <stan@stanroelofs.nl>
2023-12-01 08:35:32 +0100

    Added remaining tokens and started implementing scanner

--- include/slang/vm.h
+++ include/slang/vm.h
@@ -13,7 +13,25 @@ typedef int slang_run_result;
 #define SLANG_RUNTIME_ERROR 2
 #define SLANG_FILE_ERROR 3
 
+/**
+ * Compiles a string into bytecode and runs it.
+ * @param vm The VM to run the bytecode on.
+ * @param source The source code to compile. This must be a null-terminated string.
+ * @return SLANG_RUN_OK if the code ran successfully.
+ * @return SLANG_COMPILE_ERROR if there was a compilation error.
+ * @return SLANG_RUNTIME_ERROR if there was a runtime error.
+ */
 slang_run_result slang_run_string(slang_vm *vm, const char *source);
+
+/**
+ * Compiles a file into bytecode and runs it.
+ * @param vm The VM to run the bytecode on.
+ * @param path The path to the file to compile. This must be a null-terminated string.
+ * @returns SLANG_RUN_OK if the code ran successfully.
+ * @returns SLANG_COMPILE_ERROR if there was a compilation error.
+ * @returns SLANG_RUNTIME_ERROR if there was a runtime error.
+ * @returns SLANG_FILE_ERROR if the file could not be opened.
+ */
 slang_run_result slang_run_file(slang_vm *vm, const char *path);
 
 const char *slang_get_error(slang_vm *vm);
--- src/scanner.c
+++ src/scanner.c
@@ -1,5 +1,7 @@
 #include "scanner.h"
 
+#include "common.h"
+
 void slang_scanner_init(slang_scanner *scanner, const char *source)
 {
     scanner->token_start = source;
@@ -7,12 +9,121 @@ void slang_scanner_init(slang_scanner *scanner, const char *source)
     scanner->line = 1;
 }
 
-slang_token slang_scanner_scan_token(slang_scanner *scanner)
+static slang_token make_token(slang_scanner *scanner, slang_token_type type)
 {
     slang_token token;
-    token.type = TOKEN_ERROR;
-    token.start = scanner->token_current;
-    token.length = 1;
+    token.type = type;
+    token.start = scanner->token_start;
+    token.length = (unsigned)(scanner->token_current - scanner->token_start);
     token.line = scanner->line;
-    return token; // TODO!!
+    return token;
+}
+
+static char next_token(slang_scanner *scanner)
+{
+    return *scanner->token_current++;
+}
+
+static char peek_token(slang_scanner *scanner)
+{
+    return *scanner->token_current;
+}
+
+static bool is_at_end(slang_scanner *scanner)
+{
+    return peek_token(scanner) == '\0';
+}
+
+static void skip_whitespace(slang_scanner *scanner)
+{
+    while (true)
+    {
+        switch (peek_token(scanner))
+        {
+        case ' ':
+        case '\r':
+        case '\t':
+            next_token(scanner);
+            break;
+        case '\n':
+            scanner->line++;
+            next_token(scanner);
+            break;
+        case '/':
+            if (peek_token(scanner) == '/')
+            {
+                // A comment goes until the end of the line
+                while (peek_token(scanner) != '\n' && !is_at_end(scanner))
+                    next_token(scanner);
+            }
+            else
+            {
+                return;
+            }
+            break;
+        default:
+            return;
+        }
+    }
+}
+
+/// Peek the next token
+static bool match(slang_scanner *scanner, char expected)
+{
+    if (is_at_end(scanner))
+        return false;
+    if (*scanner->token_current != expected)
+        return false;
+
+    scanner->token_current++;
+    return true;
+}
+
+slang_token slang_scanner_scan_token(slang_scanner *scanner)
+{
+    scanner->token_start = scanner->token_current;
+
+    skip_whitespace(scanner);
+
+    if (is_at_end(scanner))
+        return make_token(scanner, TOKEN_EOF);
+
+    char c = next_token(scanner);
+    switch (c)
+    {
+    // One character tokens
+    case '(':
+        return make_token(scanner, TOKEN_LEFT_PARENTHESIS);
+    case ')':
+        return make_token(scanner, TOKEN_RIGHT_PARENTHESIS);
+    case '{':
+        return make_token(scanner, TOKEN_LEFT_BRACE);
+    case '}':
+        return make_token(scanner, TOKEN_RIGHT_BRACE);
+    case ',':
+        return make_token(scanner, TOKEN_COMMA);
+    case '.':
+        return make_token(scanner, TOKEN_DOT);
+    case '-':
+        return make_token(scanner, TOKEN_MINUS);
+    case '+':
+        return make_token(scanner, TOKEN_PLUS);
+    case ';':
+        return make_token(scanner, TOKEN_SEMICOLON);
+    case '/':
+        return make_token(scanner, TOKEN_SLASH);
+    case '*':
+        return make_token(scanner, TOKEN_STAR);
+    // One or two character tokens
+    case '!':
+        return make_token(scanner, match(scanner, '=') ? TOKEN_NEGATE_EQUAL : TOKEN_NEGATE);
+    case '=':
+        return make_token(scanner, match(scanner, '=' ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL));
+    case '>':
+        return make_token(scanner, match(scanner, '=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    case '<':
+        return make_token(scanner, match(scanner, '=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    }
+
+    return make_token(scanner, TOKEN_ERROR); // TODO
 }
